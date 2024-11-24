b99c25cb78c9106016cf7aef923b66124ad4f200
Ubuntu <ubuntu@ip-172-31-81-155.ec2.internal>
2024-11-24 12:03:28 +0000

    index.php and app.config

--- casestudy/index.php
+++ casestudy/index.php
@@ -3,6 +3,6 @@
   <title>PHP-Test</title>
  </head>
  <body>
-  <?php echo '<h1>Hello World!</h1><h3>Welcome to WPMU DEV</h3>'; ?>
+  <?php echo '<h1>Hello World!</h1><h3>Welcome to my Case Study >'; ?>
  </body>
 </html>
